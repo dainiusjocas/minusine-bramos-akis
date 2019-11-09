45ec78d6bfed869c6d9a29265e8064a2b44c2243
Dainius Jocas <dainius.jocas@gmail.com>
2019-11-09 12:32:52 +0200

    feat: proper lazy coordinate fetching

--- src/mba/archive.clj
+++ src/mba/archive.clj
@@ -1,9 +1,10 @@
 (ns mba.archive
   (:require [jsonista.core :as json]
-            [org.httpkit.client :as http])
-  (:import (java.time LocalDate)))
+            [org.httpkit.client :as http]
+            [clojure.tools.logging :as log]))
 
-(defrecord archive-record [url-key timestamp original mimetype status-code digest length])
+(defrecord archive-record
+  [url-key timestamp original mimetype status-code digest length dupecount])
 
 (defn fetch-snapshot [original timestamp]
   @(http/request
@@ -11,26 +12,46 @@
       :url    (format "http://web.archive.org/web/%s/%s" timestamp original)}
      (fn [resp] (-> resp :body))))
 
+(defn remove-nil-vals [m] (into {} (remove (comp nil? second) m)))
+
 (defn query-archive-cdx
   "Returns a list of archive-records"
   [{:keys [url limit from to offset]}]
+  (log/debugf "Limit %s offset %s" limit offset)
   @(http/request
      {:method       :get
       :url          "http://web.archive.org/cdx/search/cdx"
-      :query-params {:url    url
-                     :output "json"
-                     :from   (or from "1990")
-                     :to     (or to (inc (.getYear (LocalDate/now))))
-                     :limit  (or limit 5)
-                     :offset (or offset 0)}}
+      :query-params (remove-nil-vals
+                      {:url           url
+                       :output        "json"
+                       :from          from
+                       :to            to
+                       :limit         (or limit 5)
+                       :offset        (or offset 0)
+                       :showDupeCount true
+                       :fastLatest    true
+                       :collapse      "digest"})}
      (fn [resp]
        (let [[_ & data] (-> resp :body (json/read-value))]
          (map (fn [data-line] (apply ->archive-record data-line)) data)))))
 
-(defn fetch-n-pages
-  ([query n] (fetch-n-pages query n (min 1 n) 0))
-  ([query n step offset]
-   (prn "Step:" step "offset:" offset)
-   (when (< offset n)
-     (take n (lazy-cat (query-archive-cdx (assoc query :limit step :offset offset))
-                       (fetch-n-pages query n step (+ step offset)))))))
+(def default-step 1000)
+
+(defn fetch-coordinates
+  "Returns a lazy sequence of coordinates. Total amount of coordinates fetched can be defined
+   by :n in opts. If :n is not provided then *all* coordinates will be fetched lazily.
+   Query :limit is interpreted as a max chunk size in the iteration.
+   If query :limit is not provided then default-step is used."
+  ([query] (fetch-coordinates query {}))
+  ([{:keys [offset limit] :as query} {:keys [n previous-was-empty? fetched] :as opts}]
+   (log/debugf "Fetching %s" opts)
+   (when (and (not previous-was-empty?) (or (nil? n) (nil? fetched) (< fetched n)))
+     (let [step (if n
+                  (min (or limit default-step) (- n (or fetched 0)))
+                  (or limit default-step))
+           chunk (query-archive-cdx (assoc query :limit step :offset offset))]
+       (lazy-cat chunk (fetch-coordinates
+                         (update query :offset (fnil + 0) step)
+                         (-> opts
+                             (assoc :previous-was-empty? (empty? chunk))
+                             (update :fetched (fnil + 0) (count chunk)))))))))
